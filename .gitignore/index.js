4c7585cada2c9594f37e77200ca6918aef1fabc9
KLM19 <30330170+KLM19@users.noreply.github.com>
2018-06-28 00:51:21 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,3 +8,67 @@ bot.on("ready", function() {
 });
 
 bot.login(process.env.TOKEN);
+
+bot.on('message', message => {
+    if (message.content === prefix + "help"){
+        message.channel.sendMessage("Liste des commandes: \n - .Membres\n - .serveur \n *Ce bot contient des commandes cachées :)*");
+        console.log("Help effectué !");
+    }
+
+    if (message.content === prefix + "Membres"){
+        message.channel.sendMessage("- Lotfi (Lxtfi) 
+        "- Fadi(Kazukoo)"
+    "- Aymen(Whitiy)");
+        console.log("Membres effectué");
+    }
+
+    if (message.content === prefix + "Bot"){
+        message.channel.sendMessage("Ce bot a été crée par Lx.");
+        console.log("Bot effectué");
+    }
+
+    if (message.content === prefix + "Oh MatchiBos"){
+        message.channel.sendMessage("Chek tez Baby");
+        console.log("Oh MatchiBos effectué");
+    }
+
+    if (message.content === prefix + "serveur"){
+        message.channel.sendMessage("funcraft.net");
+        console.log("serveur effectué");
+    }
+    
+    if (message.content === prefix + "Lx"){
+        message.reply("Le roux !");
+        console.log("Lx effectué !");
+    }
+
+    if (message.content === prefix + "Kazukooo"){
+        message.channel.sendMessage("Le jaune ! :p");
+        console.log("Kazukooo effectué");
+    }
+    if (message.content === prefix + "Lotfi"){
+        message.reply("Le roux !");
+        console.log("Lotfi effectué !");
+    }
+    if (message.content === prefix + "Fadi"){
+        message.channel.sendMessage("Le jaune ! :p");
+        console.log("Fadi effectué");
+    }
+    if (message.content === prefix + "Aymen"){
+        message.channel.sendMessage("Le nwar ! :p");
+        console.log("aymen effectué");
+    }
+    if (message.content === prefix + "whitiy"){
+        message.channel.sendMessage("Le nwar ! :p");
+        console.log("whitiy effectué");
+    }
+    if (message.content === prefix + "lxtfi"){
+        message.reply("Le roux !");
+        console.log("lxtfi effectué !");
+    }
+
+
+
+
+
+});
