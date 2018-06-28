41cb7bea46734aead1ec725878a9824f873e49c0
KLM19 <30330170+KLM19@users.noreply.github.com>
2018-06-28 01:11:35 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -12,19 +12,19 @@ bot.login(process.env.TOKEN);
 
 bot.on('message', message => {
     if (message.content === prefix + "help"){
-        message.channel.sendMessage("Liste des commandes: \n - /Membres\n - /serveur \n *Ce bot contient des commandes cachées :)");
+        message.channel.sendMessage("Liste des commandes: \n - /membres\n - /serveur \n *Ce bot contient des commandes cachées :)");
         console.log("Help effectué !");
     }
-    if (message.content === prefix + "Membres"){
-        message.channel.sendMessage("- Lotfi(Lxtfi) \n Aymen(Whitiy) \n Fadi(Kazukoo)");
+    if (message.content === prefix + "membres"){
+        message.channel.sendMessage("- Lotfi(Lxtfi) \n - Aymen(Whitiy) \n - Fadi(Kazukoo)");
         console.log("Membres effectué");
     }
-    if (message.content === prefix + "Bot"){
+    if (message.content === prefix + "bot"){
         message.channel.sendMessage("Ce bot a été crée par Lx.");
         console.log("Bot effectué");
     }
 
-    if (message.content === prefix + "Oh MatchiBos"){
+    if (message.content === prefix + "oh MatchiBos"){
         message.channel.sendMessage("Chek tez Baby");
         console.log("Oh MatchiBos effectué");
     }
@@ -33,19 +33,19 @@ bot.on('message', message => {
         message.channel.sendMessage("funcraft.net");
         console.log("serveur effectué");
     }
-    if (message.content === prefix + "Kazukooo"){
+    if (message.content === prefix + "kazukooo"){
         message.channel.sendMessage("Le jaune ! :p");
         console.log("Kazukooo effectué");
     }
-    if (message.content === prefix + "Lotfi"){
+    if (message.content === prefix + "lotfi"){
         message.reply("Le roux !");
         console.log("Lotfi effectué !");
     }
-    if (message.content === prefix + "Fadi"){
+    if (message.content === prefix + "fadi"){
         message.channel.sendMessage("Le jaune ! :p");
         console.log("Fadi effectué");
     }
-    if (message.content === prefix + "Aymen"){
+    if (message.content === prefix + "aymen"){
         message.channel.sendMessage("Le nwar ! :p");
         console.log("aymen effectué");
     }
